df76a5bf758886a47b95b388e7dfe58ff4132802
marzieh <marzieh@MacBook-Air.local>
2018-03-10 12:20:19 -0500

    added JSON file to remote repo

---
+++ app.js
@@ -0,0 +1,6 @@
+var keys = {
+    consumer_key : "uOkHKRZypYPdv2zCODbCzx2MO",
+    consumer_secret : "rmXu4cdO7ETZSKTrXwmGmJKYl9SuZCj9ijYqDRhrj3deJy4PQq",
+ token : "879320047034015744-9LsyRrrw38SN4XbRNmHVuopR6GN2FEZ",
+ token_secret : "5jBBWV80Vc5zRMmEg1GReGef4cE3yDmgXdtyG4uhUY8jS"
+};
